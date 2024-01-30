85ca16147baa35561731e555b828baf95ce6023e
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-01-31 04:36:26 +0530

    Create oops5.py

---
+++ oops5.py
@@ -0,0 +1,19 @@
+class Item:
+	
+    def __init__(self):
+    	print("win")
+
+    def cal(self, a, b):
+        return a * b
+     
+
+item1 = Item()
+item1.name = "chicken"
+item1.quantity = 1
+item1.price = 200
+
+
+item2 = Item()
+item2.name = "onion"
+item2.quantity = 2
+item2.price = 30
