adf43442621c815e3a64d89fe27f11c4a14d087a
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-01-31 04:31:38 +0530

    Create oops2_1.py

---
+++ oops2_1.py
@@ -0,0 +1,9 @@
+class Item:
+    def cal(a,b):              #use self parameter
+        return a,b
+
+item1 = Item()
+item1.name = "chicken"
+item1.quantity = 1
+item1.price = 200
+print(item1.cal(item1.name))
