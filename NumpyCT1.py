40b516fcd02626849433aa65b181b7aa32884462
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-03-03 20:41:29 +0530

    Create NumpyCT1.py

---
+++ NumpyCT1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+data1=np.zeros([3,3])
+data2=np.ones([3,3])
+data3=np.full([2,2],5)
+data4=np.arange(2,20,2)
+data5=np.linspace(1,20,5)
+print(data1)
+print(data2)
+print(data3)
+print(data4)
+print(data5)
