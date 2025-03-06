d96aa8ddb8fed6a0972c6d40b1e01026c0889d3c
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-03-06 21:52:13 +0530

    Update TypeCast1.py

--- TypeCast1.py
+++ TypeCast1.py
@@ -1,9 +1,12 @@
 Id=1
 Pizzas=3.0
 Bill=200
+Cash=1
 print(str(Id))
 print(int(Pizzas))
 print(float(Bill))
+print(bool(Cash))
 print(type(Id))
 print(type(Pizzas))
 print(type(Bill))
+print(type(Cash))
