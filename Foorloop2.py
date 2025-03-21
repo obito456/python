da2f2bf5120c7a844957a74b24b2217b1c9bbe77
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-03-21 19:22:22 +0530

    Create Foorloop2.py

---
+++ Foorloop2.py
@@ -0,0 +1,4 @@
+# IF YOU HAVE A MOVIETHEATRE ,THE NO OF Audience BOOKING ON THIS DAY IS 100 AND YOU HAVE TO PRINT TICKET NO 
+
+Audience=100
+print("\n".join(f"TICKET No : {i}" for i in range(1,Audience+1)))
