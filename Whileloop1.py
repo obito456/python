bd491e9c09208579fba339f4d028854d745d0e76
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-03-21 19:31:25 +0530

    Create Whileloop1.py

---
+++ Whileloop1.py
@@ -0,0 +1,9 @@
+# IF YOU HAVE A COFFEE SHOP ,THE NO OF CUSTOMERS ON THIS DAY IS UNKNOWN AND YOU HAVE TO PRINT RECEIPT NO 
+import random
+
+Customers= random.randint(1,150)
+print(Customers)
+X=1
+while X<=Customers:
+    print("TICKET NO : ",X)
+    X+=1
