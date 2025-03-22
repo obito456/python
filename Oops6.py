12edacabf95328c2d11491bfac2df04312af31bf
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-03-22 23:01:57 +0530

    Create Oops6.py

---
+++ Oops6.py
@@ -0,0 +1,11 @@
+class Player:
+    Team="India"
+    def __init__(this,pname,Team):
+        this.Name=pname
+        this.Team=Team
+
+p1=Player("ABD","South Africa")
+print(p1.Team,p1.Name)
+p2=Player("Virat","RCB")
+print(p2.Team,p2.Name)
+
