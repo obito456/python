0f47f43330d006e2dd3519f2821415b481272fff
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-03-23 14:05:05 +0530

    Create Format2.py

---
+++ Format2.py
@@ -0,0 +1,7 @@
+price1=10.14159
+price2=15.5678
+price3=30.34
+
+print(f"The cost of Tea is = {price1:.1f}")
+print(f"The cost of Tea is = {price2:.2f}")
+print(f"The cost of Tea is = {price3:.3f}")
