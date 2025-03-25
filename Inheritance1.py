179e1b4b936805a94c61708446bad65624fe1894
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-03-25 19:09:52 +0530

    Create Inheritance1.py

---
+++ Inheritance1.py
@@ -0,0 +1,10 @@
+class Ambani:
+    def __init__(self):
+        self.Reliance="Reliance supermarket"
+        self.Jio="Jio Telecom"
+class Ananth(Ambani):
+    pass
+    
+ananth=Ananth()
+print(ananth.Reliance)
+print(ananth.Jio)
