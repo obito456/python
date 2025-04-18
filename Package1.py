c48674fa84203dd59ce0ce481c957881074e487c
vijayuchiha <150343458+obito456@users.noreply.github.com>
2025-04-18 20:05:57 +0530

    Create Package1.py

---
+++ Package1.py
@@ -0,0 +1,10 @@
+from .square import getsquare
+from .cube import getcube
+
+def getsquare(num):
+    return num**2
+def getcube(num):
+    return num**3
+
+from MathPackage.square import getsquare
+print(getsquare(5))
